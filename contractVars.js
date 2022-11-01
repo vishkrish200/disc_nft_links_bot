6cfc19657ff4a265dbbe77ff105a1a592223e0ef
Vishnu Krishnan <vishkrish200@gmail.com>
2022-11-01 20:00:34 +0530

    🛠🤞🏼

---
+++ contractVars.js
@@ -0,0 +1,39 @@
+import { ethers } from "ethers";
+import * as dotenv from "dotenv";
+import fetch from "node-fetch";
+
+dotenv.config();
+
+const provider = new ethers.providers.JsonRpcProvider(
+  "https://eth-mainnet.g.alchemy.com/v2/eeTRoWXmVY5EwhqanlW1tOhs6hXH6iYi"
+);
+
+export default async function fetchVars(caddress) {
+  const abi = await fetch(
+    `https://api.etherscan.io/api?module=contract&action=getabi&address=${caddress}&apikey=${process.env.ETHERSCAN_API_KEY}`
+  );
+  //   console.log(abi);
+  const abi_json = await abi.json();
+
+  const data = JSON.parse(abi_json.result);
+  //   console.log(data);
+
+  const emptyInputs = data.filter(
+    (item) => item["inputs"]?.length === 0 && item.stateMutability === "view"
+  );
+
+  const contract = new ethers.Contract(caddress, emptyInputs, provider);
+
+  const array = [];
+  for (const item of emptyInputs) {
+    array.push(item.name && contract[item.name]());
+  }
+
+  const fetchedValues = await Promise.all(array);
+
+  const output = fetchedValues.map((output, index) => ({
+    name: `${emptyInputs[index].name}`,
+    value: `${output}`,
+  }));
+  return output;
+}
---
+++ index.js
@@ -0,0 +1,129 @@
+import { Client, GatewayIntentBits, EmbedBuilder } from "discord.js";
+import * as dotenv from "dotenv";
+import fetchVars from "./contractVars.js";
+import { ethers } from "ethers";
+import fetch from "node-fetch";
+
+dotenv.config();
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+client.on("messageCreate", async (message) => {
+  if (message.author.bot) return;
+
+  if (message.content.startsWith("0x")) {
+    try {
+      const contract_address = message.content.slice(0, 42);
+      const options = {
+        method: "GET",
+        headers: { "X-API-KEY": process.env.OS_API_KEY },
+      };
+      //   console.log(typeof message.content);
+      const response = await fetch(
+        `https://api.opensea.io/api/v1/asset_contract/${message.content}`,
+        options
+      );
+      const data = await response.json();
+      const os_collection_slug = data.collection?.slug;
+      let collection_name = "";
+      let collection_description = "";
+      let collection_website = "";
+      let collection_discord = "";
+      let collection_twitter = "";
+      if (os_collection_slug != null) {
+        collection_name = data.name ? data.name : "";
+        collection_description = data.description ? data.description : "";
+        collection_website = data.collection.external_url
+          ? data.collection.external_url
+          : "";
+        collection_discord = data.collection.discord_url
+          ? data.collection.external_url
+          : "";
+        collection_twitter = data.collection.twitter_username
+          ? `https://twitter.com/${data.collection.twitter_username}`
+          : "";
+      }
+      const values = await fetchVars(contract_address);
+      //   console.log(values);
+
+      const embed = new EmbedBuilder()
+        .setColor(0x51ff00)
+        .setTitle(
+          `${collection_name ? collection_name : "Does not have a name"}`
+        )
+        .setDescription(
+          `${
+            collection_description
+              ? collection_description
+              : "Does not have a description"
+          }`
+        )
+        .setThumbnail(
+          `${
+            data.image_url
+              ? data.image_url
+              : "https://play-lh.googleusercontent.com/ovFbGElmfBf5gqcNhKLDkNIMMf_54hJ02G6lNTQFYsmK4rqwBjKrbl24RiAPOLiVkdk"
+          }`
+        )
+        .addFields({ name: "Contract Address", value: `${message.content}` })
+        .setTimestamp()
+        .setFooter({
+          text: "by your fat mom",
+          iconURL:
+            "https://play-lh.googleusercontent.com/ovFbGElmfBf5gqcNhKLDkNIMMf_54hJ02G6lNTQFYsmK4rqwBjKrbl24RiAPOLiVkdk",
+        });
+      values.forEach((item) => {
+        embed.addFields({
+          name: `${item.name ? item.name : "empty"}`,
+          value: `${item.value ? item.value : "empty  "}`,
+          inline: true,
+        });
+      });
+      embed
+        .addFields({
+          name: `Links`,
+          value: ` [Etherscan](https://etherscan.io/address/${
+            message.content
+          }) | [NFTNerds](https://nftnerds.ai/collection/${
+            message.content
+          }/liveview) | [Blur](https://blur.io/collection/${
+            message.content
+          }) | [Opensea](https://opensea.io/collection/${
+            os_collection_slug ? os_collection_slug : ""
+          }) | [X2Y2](https://x2y2.io/collection/${
+            message.content
+          }) | [Degenmint](https://degenmint.xyz/?address=${message.content})`,
+        })
+        .addFields({
+          name: `Collection Links`,
+          value: ` [Website](${collection_website}) | [Twitter](${collection_twitter}) | [Discord](${collection_discord})`,
+        });
+      message.reply({ embeds: [embed] });
+      console.log("success");
+    } catch (error) {
+      console.log("fail");
+      const errorEmbed = new EmbedBuilder()
+        .setColor(0xff0000)
+        .setTitle("An Error Occured")
+        .setDescription(
+          "Either \nthe contract entered is invalid \ncollection does not have an os page\npls contact SlimyLemon#8762"
+        )
+        .setTimestamp()
+        .setFooter({
+          text: "by your fat mom",
+          iconURL:
+            "https://play-lh.googleusercontent.com/ovFbGElmfBf5gqcNhKLDkNIMMf_54hJ02G6lNTQFYsmK4rqwBjKrbl24RiAPOLiVkdk",
+        });
+      message.reply({ embeds: [errorEmbed] });
+    }
+  }
+});
+
+client.login(process.env.DISC_BOT_TOKEN);
